c61c838cc9678bbbf52b2f2957fb1fb51a54f1ab
iandennismiller@gmail.com <iandennismiller@gmail.com@45f28340-1d62-8a7f-c204-77b0ce2c84e1>
2011-01-20 05:06:42 +0000

    * wrote parsing class * simple fft data test parses both test data files * added wiki extern to project * not going to use directory-style class - put everything in one file instead
    git-svn-id: http://asa-utils.googlecode.com/svn/trunk@5 45f28340-1d62-8a7f-c204-77b0ce2c84e1

---
+++ lib/asa_utils.py
@@ -0,0 +1,103 @@
+from __future__ import with_statement
+import csv, sys, re
+
+class parse(object):
+    
+    def __init__(self, filename):
+        with open(filename) as f:
+            data_file = csv.reader(f, delimiter='\t')
+            self.read_file(data_file)
+            
+    def read_file(self, data_file):
+        # verify data export file type
+        ident = data_file.next()[0]
+        if ident != '# Time Frequency Data Export':
+            print "data file does not appear to be Time Frequency Data"
+            sys.exit()
+            
+        # skip metadata
+        for i in range(0, 8):
+            data_file.next()
+        
+        # determine number of bands
+        band_count_line = data_file.next()
+        if band_count_line[0] == 'NumberOfBands=':
+            self.band_count = int(band_count_line[1])
+        else:
+            print "unable to determine number of bands in data"
+            sys.exit()
+        
+        # remove BandsData
+        data_file.next()
+        # remove BandsData ranges
+        for i in range(0, self.band_count):
+            data_file.next()
+        # remove newline
+        data_file.next()
+
+        if data_file.next()[0] != 'BandsNames':
+            print "unable to determine name of bands in data"
+            sys.exit()
+        
+        self.bands_names = []
+        for i in range(0, self.band_count):
+            self.bands_names.append(data_file.next()[0].replace(' ', '_'))
+        
+        # determine number of channels
+        channel_count_line = data_file.next()
+        if channel_count_line[0] == 'NumberOfChannels=':
+            self.channel_count = int(channel_count_line[1])
+        else:
+            print "unable to determine number of channels in data"
+            sys.exit()
+        
+        # skip unit measure
+        data_file.next()
+        # skip ValuesTransposed
+        data_file.next()
+
+        # read values
+        self.values = []
+        for i in range(0, self.band_count):
+            values_read = data_file.next()
+            if re.search(r'\:$', values_read[0]):
+                values_read.pop(0)
+            for j in range(0, self.channel_count):
+                self.values.append(values_read[j])
+
+        # skip number of data sets
+        data_file.next()
+        data_file.next()
+        
+        # get filename
+        data_filename = data_file.next()[0]
+
+        # skip Labels
+        data_file.next()
+
+        self.labels = []
+        # read Label names
+        labels_read = data_file.next()
+        for i in range(0, self.channel_count):
+            self.labels.append(labels_read[i])
+            
+        # amalgamate labels
+        if self.band_count > 1:
+            self.amalgamate_labels = []
+            for band_name in self.bands_names:
+                for label in self.labels:
+                    self.amalgamate_labels.append('%s_%s' % (band_name, label))
+        else:
+            self.amalgamate_labels = self.labels
+        
+    def as_hash(self):
+        h = {
+            'band_count': self.band_count,
+            'bands_names': self.bands_names,
+            'channel_count': self.channel_count,
+            'labels': self.labels,
+            'values': self.values,
+            'amalgamate_labels': self.amalgamate_labels,
+        }
+        return h
+    
---
+++ t/test_fft_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('/Users/idm/Code/academic/asa-utils/lib')
+from asa_utils import parse
+
+f = '/Users/idm/Code/academic/asa-utils/t/data/single_band_test.asc'
+t = parse(f)
+print t.as_hash()
+
+f = '/Users/idm/Code/academic/asa-utils/t/data/multiple_band_test.asc'
+t = parse(f)
+print t.as_hash()
