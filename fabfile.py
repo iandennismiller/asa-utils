7914418bdf8f8c2420bb3c7a7ff85a52160abc94
iandennismiller@gmail.com <iandennismiller@gmail.com@45f28340-1d62-8a7f-c204-77b0ce2c84e1>
2011-01-21 16:42:47 +0000

    * the authoritative version is set in setup.py * StartHere is now version-agnostic * StartHere includes UNIX magic to autodetect version number * fabfile will detect version for tagging operation
    git-svn-id: http://asa-utils.googlecode.com/svn/trunk@23 45f28340-1d62-8a7f-c204-77b0ce2c84e1

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,7 @@ def sdist():
     print local('cp wiki/ReadMe.wiki doc/README.TXT; cp wiki/StartHere.wiki doc/INSTALL.TXT')
     print local('rm -rf build; python setup.py sdist --formats=gztar,zip')
 
-def tag(ver):
+def tag():
+    ver = local('python setup.py --version').rstrip()
     cmd = 'svn cp https://asa-utils.googlecode.com/svn/trunk/ https://asa-utils.googlecode.com/svn/tags/%s/'
     os.system(cmd % ver)
